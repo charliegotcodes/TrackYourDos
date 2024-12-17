37bd22b7bd11840d3ac99d9453eab5d0c4d32499
charliegotcodes <kevin.charles0164@gmail.com>
2024-12-16 20:26:06 -0500

    Initial Commit

---
+++ TrackYourDo's/TrackYourDo_sApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackYourDo_sApp.swift
+//  TrackYourDo's
+//
+//  Created by Kevin Charles on 2024-12-16.
+//
+
+import SwiftUI
+
+@main
+struct TrackYourDo_sApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrackYourDo'sTests/TrackYourDo_sTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackYourDo_sTests.swift
+//  TrackYourDo'sTests
+//
+//  Created by Kevin Charles on 2024-12-16.
+//
+
+import Testing
+@testable import TrackYourDo_s
+
+struct TrackYourDo_sTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
